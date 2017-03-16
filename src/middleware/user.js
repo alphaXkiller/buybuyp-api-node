b336780fbbf61cdd51a9961789de746eb283c922
Franklin Huang <frankie0628@gmail.com>
2017-03-16 13:14:54 -0700

    fix user save

--- src/middleware/user.js
+++ src/middleware/user.js
@@ -7,13 +7,13 @@ const addIfNotFound = async (ctx, next) => User
   .getByUid(ctx.mysql, {uid: ctx.params.uid})   
 
   // TODO: grab info from checker
-  .tap(R.when(R.isNil, () => User.save(ctx.mysql, {
+  .tap(R.when(R.isNil, () => User.save(ctx.mysql, [{
     uid: ctx.request.body.uid,
     name: ctx.request.body.name,
     email: ctx.request.body.email,
     email_verified: ctx.request.body.email_verified ? 1 : 0,
     profile_image: ctx.request.body.profile_image
-  })))
+  }])))
 
   .then( () => { ctx.body={ success: true } } )
 
